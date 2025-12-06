0ffc3e2374d6bcea5e17086baece896da8e2f4cf
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-06 09:05:25 -0500

    Update Testing.cs

--- Weighted Directed Graph/Testing.cs
+++ Weighted Directed Graph/Testing.cs
@@ -1,14 +1,25 @@
-class Testing{
-    public static void Main(string[] args){
-    Graph building = new Graph();
-    building.InitializeBuilding();
-
-    List<Node> path = Dijkstra.FindShortestPath(building, "Rm 301", "Exit B");
-
-    Console.WriteLine("Path:");
-    foreach (var node in path)
+class Testing
+{
+    public static void Main(string[] args)
     {
-        Console.WriteLine(node.Name);
-    }
+        Graph building = new Graph();
+        building.InitializeBuilding();
+
+        Direction direction = new Direction();
+        List<Node> path = Dijkstra.FindShortestPath(building, "Rm 302", "Rm 317");
+
+        Console.WriteLine("Path:");
+        foreach (var node in path)
+        {
+            Console.WriteLine(node.Name);
+        }
+
+        List<string> directions = direction.BuildDirections(path);
+
+        Console.WriteLine("\nDirections:");
+        foreach (var step in directions)
+        {
+            Console.WriteLine(step);
+        }
     }
 }
