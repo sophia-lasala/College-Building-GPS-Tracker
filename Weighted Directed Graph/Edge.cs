86c9ce4f2289658955cc46f057c61621890e9fc4
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-09 11:12:09 -0500

    Update Edge.cs

--- Weighted Directed Graph/Edge.cs
+++ Weighted Directed Graph/Edge.cs
@@ -1,10 +1,11 @@
 class Edge
 {
-    public Node From { get; }
+    //Edge based on neighboring nodes and the weight
+    public Node From { get; } 
     public Node To { get; }
     public double Weight { get; }
 
-    public Edge(Node from, Node to, double weight)
+    public Edge(Node from, Node to, double weight) //constructor to create Edge object
     {
         From = from;
         To = to;
