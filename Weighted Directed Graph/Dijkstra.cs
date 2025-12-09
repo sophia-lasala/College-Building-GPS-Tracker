a3bdf8ba94d9c3a609c3bfe5f4bda64055e54b39
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-09 11:01:33 -0500

    Update Dijkstra.cs
    Added comments

--- Weighted Directed Graph/Dijkstra.cs
+++ Weighted Directed Graph/Dijkstra.cs
@@ -1,70 +1,71 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
+//implements basic data structures 
 
-class Dijkstra
+class Dijkstra //based on Dijkstra's algorithm 
 {
-    public static List<Node> FindShortestPath(Graph graph, string startName, string endName)
+    public static List<Node> FindShortestPath(Graph graph, string startName, string endName) //needs to be called for pathmaking
     {
-        var nodes = graph.GetAllNodes();
-        var unvisited = new HashSet<Node>();
+        var nodes = graph.GetAllNodes(); //store all nodes from graph within nodes variable
+        var unvisited = new HashSet<Node>(); //creates hashset per node marking it either visited or unvisited 
 
-        foreach (var node in nodes.Values)
+        foreach (var node in nodes.Values) //set for each node value, make sure no data from previous searches are left
         {
-            node.Distance = double.PositiveInfinity;
-            node.Previous = null;
-            unvisited.Add(node);
+            node.Distance = double.PositiveInfinity; //have an automatic high positive number to ensure paths always work
+            node.Previous = null; //no previous node
+            unvisited.Add(node); //add to HashSet
         }
 
-        Node start = graph.GetNode(startName);
+        Node start = graph.GetNode(startName); // Node = (startName, endName, distance) 
         Node end = graph.GetNode(endName);
         start.Distance = 0;
 
-        while (unvisited.Count > 0)
+        while (unvisited.Count > 0) // continues into all nodes needed are accessed 
         {
-            Node? current = null!;
-            double smallestDistance = double.PositiveInfinity;
-            foreach (var n in unvisited)
+            Node? current = null!; //reset to default after each node
+            double smallestDistance = double.PositiveInfinity; //reset to default after each node
+            foreach (var n in unvisited) // each node in the hash set marked as unvisitied 
             {
-                if (n.Distance < smallestDistance)
+                if (n.Distance < smallestDistance) //if node's distance is smaller it becomes the smallest distance until the best route is formed
                 {
                     smallestDistance = n.Distance;
                     current = n;
                 }
             }
 
-            if (current == null!)
+            if (current == null!) //means node is not apart of the optimal route
                 break;
 
-            if (current == end)
+            if (current == end) //reached the end node specified by the user
                 break;
 
-            unvisited.Remove(current);
+            unvisited.Remove(current); //removes from hashset so it's not selected again 
 
-            foreach (var edge in current.OutgoingEdges)
+            foreach (var edge in current.OutgoingEdges) // calls list from Node class, accessed by Graph class when adding edges 
             {
-                Node neighbor = edge.To;
-                if (!unvisited.Contains(neighbor))
+                Node neighbor = edge.To; 
+                if (!unvisited.Contains(neighbor)) //if neighbor isn't contained in unvisited HashSet
                     continue;
 
-                double previous = current.Distance + edge.Weight;
-                if (previous < neighbor.Distance)
+                double previous = current.Distance + edge.Weight; //all edge weights are 1 for our purposes so current Distance + 1 
+                if (previous < neighbor.Distance) // if previous is less distance then neighbor 
                 {
-                    neighbor.Distance = previous;
-                    neighbor.Previous = current;
+                    neighbor.Distance = previous; // then previous becomes distance
+                    neighbor.Previous = current; // current is set to previous fo next node
                 }
             }
         }
 
-        var path = new List<Node>();
-        Node pathNode = end;
-        while (pathNode != null)
+        var path = new List<Node>(); // where the path made is stored 
+        Node pathNode = end; 
+        while (pathNode != null) // if the last node isn't reached 
         {
-            path.Add(pathNode);
-            pathNode = pathNode.Previous;
+            path.Add(pathNode); // add to list
+            pathNode = pathNode.Previous; //goes from end to start of path
         }
 
-        path.Reverse();
-        return path;
+        path.Reverse(); //reverses so its start to end
+        return path; //returns final path
     }
 
     
