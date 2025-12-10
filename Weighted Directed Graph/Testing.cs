1e00d1439ed63826054dedb717d13588d073fd35
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-09 19:27:18 -0500

    Update Testing.cs

--- Weighted Directed Graph/Testing.cs
+++ Weighted Directed Graph/Testing.cs
@@ -4,10 +4,11 @@ class Testing
     {
         Graph building = new Graph(); //object for Graph class
         building.InitializeBuilding(); //populates with all nodes and edges necessary for building
-
+        HistoryManager historyManager = new HistoryManager(); //object for HistoryManager class
         Direction direction = new Direction(); //object for Direction class 
         List<Node> path = Dijkstra.FindShortestPath(building, "Rm 302", "Rm 317"); //the rooms would be user inputted in reality 
         //basically Dijkstra.FindShortestPath(building, startNode, endNode) essential for path making
+        historyManager.AddEntry("Rm 302", "Rm 317", path); //adds to history as a stack entry
 
         Console.WriteLine("Path:"); 
         foreach (var node in path)  //not needed just proof of where each path goes
@@ -22,6 +23,13 @@ class Testing
         foreach (var step in directions) //what would be used for the directions on the app 
         {
             Console.WriteLine(step);
+        }
+
+        historyManager.GetList(); //gets the history list of previous paths taken
+        Console.WriteLine("\nHistory:");
+        foreach (var entry in historyManager.GetList()) //displays the history of paths taken
+        {
+            Console.WriteLine(entry.ToString());
         }
     }
 }
