c1163ba4250dbd4860200a6589753cd73bb80947
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-09 19:31:01 -0500

    Update HistoryManager.cs

--- Weighted Directed Graph/HistoryManager.cs
+++ Weighted Directed Graph/HistoryManager.cs
@@ -2,13 +2,14 @@ class HistoryManager
 {
     private Stack<History> historyStack = new Stack<History>();
 
-    public void AddEntry(string start, string end, List<Node>? path)
+    public void AddEntry(string start, string end, List<Node>? path) //it should already grab the previous path so it doesn't need to be rerun
+        //if it doesn't just make some variables and rerun the FindShortestPath command
     {
         History history = new History(start, end, path);
         historyStack.Push(history);
     }
 
-     public List<History> GetList()
+     public List<History> GetList() //*important* converts the stack to a list so when a user clicks on an entry, its not limited by stack rules
     {
         return historyStack.ToList(); 
     }
