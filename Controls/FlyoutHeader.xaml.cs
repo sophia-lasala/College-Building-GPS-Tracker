d7586bab2113a81fcc6ff3b86f2eb24846393ba5
shaliiid <131724369+shaliiid@users.noreply.github.com>
2025-12-19 12:42:59 -0500

    Add files via upload

---
+++ Controls/FlyoutHeader.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSTrackerNYIT.Controls
+{
+    public partial class FlyoutHeader : ContentView
+    {
+        public FlyoutHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
