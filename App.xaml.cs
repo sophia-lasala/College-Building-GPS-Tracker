16e4e31f49c403cb5fdb5399b883db7900a0728b
shaliiid <131724369+shaliiid@users.noreply.github.com>
2025-12-19 12:47:16 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using GPSTrackerNYIT.Views;
+
+namespace GPSTrackerNYIT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        public static HistoryManager HistoryManager { get; } = new HistoryManager();
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Input;
+
+namespace GPSTrackerNYIT
+{
+    public partial class AppShell : Shell
+    {
+        public Dictionary<string, Type> Routes { get; private set; } = new Dictionary<string, Type>();
+        public ICommand HelpCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+        public AppShell()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+
+        //protected override void OnNavigating(ShellNavigatingEventArgs args)
+        //{
+        //    base.OnNavigating(args);
+
+        //    // Cancel any back navigation
+        //    //if (e.Source == ShellNavigationSource.Pop)
+        //    //{
+        //    //    e.Cancel();
+        //    //}
+        //}
+
+        //protected override void OnNavigated(ShellNavigatedEventArgs args)
+        //{
+        //    base.OnNavigated(args);
+
+        //    // Perform required logic
+        //}
+    }
+}
